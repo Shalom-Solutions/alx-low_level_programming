7d6339ef3725357bfdae42598d52b2924b419329
Shalom-solutions <omogoyejuwon@gmail.com>
2022-09-09 14:04:00 -0700

    convince

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
-#include<stdio.h>
-	
-	int main()
+#include <stdio.h>
 
-	{
-		/*Programming in C*/
-		char mystr[]= "\"Programming is like building a multilingual puzzle";
-		puts(mystr);
-		return 0;
-	}
+/**
+ *  *main - Entry point
+ *   *Return: Always 0 (Success)
+ *    */
+
+int main(void)
+{
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
+}
