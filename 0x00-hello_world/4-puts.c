9f82b0ee9b55dfed9f74c3d097727b8bea48c1f0
Shalom-solutions <omogoyejuwon@gmail.com>
2022-09-09 13:50:52 -0700

    commit changes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
-#!/bin/bash
-#Include <stdio.h>
-int main()
-{
-	/*Programming in C*/
-	char mystr[]="\"Programming is like building a multilingual puzzle";
-	puts(mystr);
-	return 0;
-}
+#include<stdio.h>
+	
+	int main()
+
+	{
+		/*Programming in C*/
+		char mystr[]= "\"Programming is like building a multilingual puzzle";
+		puts(mystr);
+		return 0;
+	}
