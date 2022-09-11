764754857beba76903270f476cfda12ed290912e
Shalom-solutions <omogoyejuwon@gmail.com>
2022-09-10 20:52:07 -0700

    2-pring _alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * printing the alphabets in lowercase
+ * Return: 0 (Success/Correct)
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+		return (0);
+}
