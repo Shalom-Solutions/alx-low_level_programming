45e40f9a8a6af414b01018a7a18f2517f2814d1d
Shalom-solutions <omogoyejuwon@gmail.com>
2022-09-11 05:57:12 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Printing all numbers with putchar
+ * Return: Always 0(Success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+
+	return (0);
+}
