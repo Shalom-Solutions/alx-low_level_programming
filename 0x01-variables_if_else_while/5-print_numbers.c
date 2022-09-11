e7608f42d0bab68ee1ab39442a1b4ff1c3502261
Shalom-solutions <omogoyejuwon@gmail.com>
2022-09-11 05:34:08 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlid.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Program to print out all single numbers between 0 and 9
+ * Return: Always 0(Success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+	printf("%d\n", num);
+	}
+	return (0);
+}
+
