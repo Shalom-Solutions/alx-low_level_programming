efdc60f5fd7dfcbb72908d810b1c61e540fa9f56
Shalom-solutions <omogoyejuwon@gmail.com>
2022-09-14 15:53:26 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 
 /**
  * main - entry point of the program
@@ -16,7 +16,6 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-	_putchar(' ');
 	_putchar('\n');
 
 
