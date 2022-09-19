57181a1acea3d323f2512c9be56689885b5582a9
Shalom-solutions <omogoyejuwon@gmail.com>
2022-09-19 14:24:55 -0700

    3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,25 +1,16 @@
 #include "main.h"
 
 /**
- * puts2 - prints one char out of 2 of a string
- * followed by a new line
- * @str: string to print the chars from
+ * _puts - prints a string to stdout
+ * @str: pointer to the string to print
  */
-void puts2(char *str)
+void _puts(char *str)
 {
-	int len, i;
+	int i;
 
-	len = 0;
-
-	while (str[len] != '\0')
-	{
-		len++;
-	}
-
-	for (i = 0; i < len; i += 2)
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		_putchar(str[i]);
 	}
-
 	_putchar('\n');
 }
